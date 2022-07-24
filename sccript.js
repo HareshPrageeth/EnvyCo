15a1b8ab04810d3dfadba4c4eb5a1d65564be505
HareshPrageeth <89728171+HareshPrageeth@users.noreply.github.com>
2022-07-25 00:02:21 +0530

    Update sccript.js

--- sccript.js
+++ sccript.js
@@ -1,42 +1,58 @@
-// var link11=null;
-// chrome.tabs.query({
-// active: true,
-// lastFocusedWindow: true
-// }, function(tabs) {
-// // and use that tab to fill in out title and url
-// var tab = tabs[0];
-// alert(tabs[0].url);
-// link11=tab.url;
-// // var urls = undefined;
-// // chrome.storage.session.get(['urls'], function(result) {
-// //   if (result)
-// //     urls = JSON.parse(result);
-// //   else 
-// //     urls = []
-// // });
-// // urls = urls.concat([tab.url]);
-// // alert(urls);
-// // chrome.storage.session.set({urls: JSON.stringify(urls)}, function() {
-// // });
+var link11=null;
+chrome.tabs.query({
+active: true,
+lastFocusedWindow: true
+}, function(tabs) {
+var tab = tabs[0];
+alert(tabs[0].url);
+link11=tab.url;
 
-// // alert(urls[0]);
-// link11=tab.url;
+fetch(link11)
+.then(response => {
+  var page = document.createElement('html');
+  page.innerHTML = response;
+  name=page.getElementById("productTitle").innerHtml;
+  dist=page.getElementByClassName("prodDetAttrValue").innerHTML;
+  dim=page.getElementByClassName("a-size-base prodDetAttrValue").innerHTML
+})
+.then(jsondata => console.log(jsondata));
+ });
+var l=dim[0];
+var b=dim[1];
+var h=dim[2];
 
-
-// fetch(link11)
-// .then(response => {
-//   var page = document.createElement('html');
-//   page.innerHTML = response;
-//   alert( page.getElementByClassName("prodDetAttrValue").innerHTML);
-// })
-// .then(jsondata => console.log(jsondata));
-//  });
+const ProdNames = [];
+const WaterUsed = [];
+const PlasticUsed = [];
+const TreesCut= [];
+const CarbonEmission = [];
+const FinalRank= [];
 
 
+var ProdNames = name
 var PlasticUsed = 2*((l*b)+(l*h)+(b*h));
 var CarbonEmission = 180.5*dist;
 var WaterUsed = PlasticUsed * 0.45469576719;
 l= l + 0.5;
 h= h + 0.5;
 b= b + 0.5;
-var TreesCut = 2*((l*b)+(l*h)+(b*h));
+var TreesCut = 2*((l*b)+(l*h)+(b*h))*0.000000029116344;
+
+
+function resetvals() {
+  ProdNames = [];
+  WaterUsed = [];
+  PlasticUsed = [];
+  TreesCut= [];
+  CarbonEmission = [];
+  FinalRank= [];
+}
+
+function addlist() {
+  ProdNames.push(prodname);
+  WaterUsed.push(WaterUse);
+  PlasticUsed.push(PlasticUse);
+  TreesCut.push(TreesCu);
+  CarbonEmission.push(CarbonEmissio);
+
+}
